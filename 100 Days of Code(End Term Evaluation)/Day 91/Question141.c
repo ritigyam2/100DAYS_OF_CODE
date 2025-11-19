011c053fe291ae9f15d69513ba85730586972de2
ritigyam2 <ritigyam2@gmail.com>
2025-11-20 00:11:18 +0530

    commit change

---
+++ 100 Days of Code(End Term Evaluation)/Day 91/Question141.c
@@ -0,0 +1,27 @@
+//(Enum)Define a structure Student with name, roll_no, and marks, then read and print one student's data.
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    // Taking input
+    printf("Name: ");
+    scanf("%s", s.name);
+
+    printf("Roll: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Marks: ");
+    scanf("%f", &s.marks);
+
+    // Output
+    printf("Name: %s | Roll: %d | Marks: %.0f\n", s.name, s.roll_no, s.marks);
+
+    return 0;
+}
--- C PROGRAMMING PRACTICE/helloworld.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(){
-    printf("Hello, World!\n");
-    return 0; //returning 0 indicates successful completion and lets us know the program ended with 0 errors
-}
